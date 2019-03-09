8e0ac47babba85d85a56705ff883c5749c969ec3
KrishnaAlagiri <krishna.alagiri03@gmail.com>
2019-03-10 01:50:57 +0530

    Add "About" dialog, "Info" icon and link redirection

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:english_words/english_words.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primaryColor: Colors.red,
       ),
-      home: RandomWords(),
+      home: /*Home(),*/RandomWords(),
     );
   }
 }
@@ -21,18 +23,112 @@ class RandomWordState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
   final Set<WordPair> _saved = new Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
+  static const String flutterUrl = 'https://flutter.io/';
+  static const String githubUrl = 'https://github.com/KrishnaAlagiri/Infi-Name';
+  TapGestureRecognizer _flutterTapRecognizer;
+  TapGestureRecognizer _githubTapRecognizer;
+
+  static const TextStyle linkStyle = const TextStyle(
+    color: Colors.blue,
+    decoration: TextDecoration.underline,
+  );
 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Name Generator'),
         actions: <Widget>[
-          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved)
+          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+          new IconButton(icon: const Icon(Icons.info), onPressed: _showDialog),
         ],
+
       ),
       body: _buildSuggestions(),
     );
   }
+
+  void _openUrl(String url) async {
+    // Close the about dialog.
+    Navigator.pop(context);
+
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  void _showDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => _buildAboutDialog(context),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _flutterTapRecognizer = new TapGestureRecognizer()
+      ..onTap = () => _openUrl(flutterUrl);
+    _githubTapRecognizer = new TapGestureRecognizer()
+      ..onTap = () => _openUrl(githubUrl);
+  }
+
+  @override
+  Widget _buildAboutDialog(BuildContext context) {
+    return new AlertDialog(
+      title: const Text('About'),
+      content: new Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _buildAboutText(),
+        ],
+      ),
+      actions: <Widget>[
+        new FlatButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          textColor: Theme
+              .of(context)
+              .primaryColor,
+          child: const Text('Okay, got it!'),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildAboutText() {
+    return new RichText(
+      text: new TextSpan(
+        text:
+        'A simple mobile app developed using Flutter that generates proposed names for Starups, Events etc. Supports both Android and iOS. \n\n',
+        style: const TextStyle(color: Colors.black87),
+        children: <TextSpan>[
+          const TextSpan(
+              text: 'The app was developed by Krishna Alagiri (KK) with '),
+          new TextSpan(
+            text: 'Flutter',
+            recognizer: _flutterTapRecognizer,
+            style: linkStyle,
+          ),
+          const TextSpan(
+            text: ' and it\'s open source; check out the source '
+                'code yourself from ',
+          ),
+          new TextSpan(
+            text: 'https://github.com/KrishnaAlagiri/Infi-Name',
+            recognizer: _githubTapRecognizer,
+            style: linkStyle,
+          ),
+          const TextSpan(text: '.'),
+        ],
+      ),
+    );
+  }
+
+
 
   void _pushSaved() {
     Navigator.of(context).push(
@@ -105,3 +201,5 @@ class RandomWordState extends State<RandomWords> {
 class RandomWords extends StatefulWidget {
   RandomWordState createState() => new RandomWordState();
 }
+
+
